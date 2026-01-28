7bc236e0fa7fa9f9daf0ce32fddedde5a6f22537
Tshepiso <ndimabuhlebakhe@gmail.com>
2026-01-28 10:56:12 +0200

    add changes made to Program.cs

--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9a670e011028efaebac4b1537fd2acd2a60e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412eec5e77f16038c05f7b98435c6341b5b60755")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
